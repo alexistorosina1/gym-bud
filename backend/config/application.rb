b4763ea4d934a2b27126d15a33c8111705b8d929
Alexis Torosina <alexistorosina1@gmail.com>
2023-08-01 10:33:59 -0400

    api to 4000

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -14,7 +14,7 @@ module Backend
     # CORS configuration
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:3000"
+        origins "http://localhost:4000"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
