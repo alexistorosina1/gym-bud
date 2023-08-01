4e33b9f0a25ab0c321887d60e6c95b88bb2ff8b1
Alexis Torosina <alexistorosina1@gmail.com>
2023-08-01 16:49:59 -0400

    devise sign up

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :workouts
   resources :comments
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations/registrations", 
+    sessions: "users/sessions",
+    passwords: "users/passwords"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
