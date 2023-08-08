c930bbeeae6194cfa734f0d4b35f3513a41f04ba
Alexis Torosina <alexistorosina1@gmail.com>
2023-08-07 21:27:06 -0400

    removed duplicates

--- backend/app/controllers/users/registrations/registrations_controller.rb
+++ backend/app/controllers/users/registrations/registrations_controller.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
-
 class Users::Registrations::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   def create
     user = User.new(user_params)
-
     if user.save
-      sign_in(user)
+      sign_in(:user, user)
       render json: { user: user, message: "Account created successfully" }, status: :created
     else
       clean_up_passwords user
@@ -19,7 +17,7 @@ class Users::Registrations::RegistrationsController < Devise::RegistrationsContr
   private
 
    def user_params
-    params.permit(:email, :password, :password_confirmation, :username, :dob, :location)
+    params.require(:user).permit(:email, :password, :password_confirmation, :username, :dob, :location)
    end
 
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
 
-    def create
-        user = User.create!(user_params)
-        render json: user, status: :created
-    end
+    # def create
+    #     user = User.create!(user_params)
+    #     render json: user, status: :created
+    # end
 
-    private
+    # private
 
-    def user_params
-        params.require(:user).permit(:username, ,:email, :password, :password_confirmation, :dob, :location)
-    end
+    # def user_params
+    #     params.require(:user).permit(:username,:email, :password, :password_confirmation, :dob, :location)
+    # end
 end
