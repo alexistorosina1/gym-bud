e34bb59ac804a8adad4b45619822b0bcbfcd1c74
Alexis Torosina <alexistorosina1@gmail.com>
2023-08-07 21:52:40 -0400

    deleted files

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   resources :workouts
   resources :comments
   resources :posts
-  devise_for :users, controllers: {
-    registrations: "users/registrations/registrations", 
-  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
